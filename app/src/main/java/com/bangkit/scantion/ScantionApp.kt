a99375e08ef4f5222a7327fdc474772d1a97baff
Wahyu Patriaji <wahyupatriaji@gmail.com>
2023-06-02 12:14:13 +0700

    change examination button in navbar

--- app/src/main/java/com/bangkit/scantion/ScantionApp.kt
+++ app/src/main/java/com/bangkit/scantion/ScantionApp.kt
@@ -2,13 +2,20 @@ package com.bangkit.scantion
 
 import android.annotation.SuppressLint
 import android.app.Application
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Create
 import androidx.compose.material.icons.rounded.Create
 import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -17,9 +24,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -81,29 +90,44 @@ fun NavBar(
         ),
     )
 
-    NavigationBar{
-        navBarItems.forEach { item ->
-            val selected = item.route == backStackEntry.value?.destination?.route
+    Box(contentAlignment = Alignment.Center) {
+        NavigationBar{
+            navBarItems.forEach { item ->
+                val selected = item.route == backStackEntry.value?.destination?.route
 
-            NavigationBarItem(
-                selected = selected,
-                onClick = {
-                    if (!selected) {
-                        if (item.name != "Periksa") navController.popBackStack()
-                        navController.navigate(item.route)
+                NavigationBarItem(
+                    selected = selected,
+                    onClick = {
+                        if (!selected) {
+                            if (item.name != "Periksa"){
+                                navController.popBackStack()
+                                navController.navigate(item.route)
+                            }
+                        }
+                    }, label = {
+                        if (item.name != "Periksa"){
+                            Text(
+                                text = item.name,
+                                fontWeight = FontWeight.SemiBold,
+                            )
+                        }
+                    }, icon = {
+                        if (item.name != "Periksa"){
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = "${item.name} Icon",
+                            )
+                        }
                     }
-                }, label = {
-                    Text(
-                        text = item.name,
-                        fontWeight = FontWeight.SemiBold,
-                    )
-                }, icon = {
-                    Icon(
-                        imageVector = item.icon,
-                        contentDescription = "${item.name} Icon",
-                    )
-                }
-            )
+                )
+            }
+        }
+        
+        Button(
+            modifier = Modifier.fillMaxWidth(.28f).height(60.dp),
+            onClick = { navController.navigate(navBarItems[1].route) },
+        ) {
+            Icon(navBarItems[1].icon, contentDescription = "create examination")
         }
     }
 }
